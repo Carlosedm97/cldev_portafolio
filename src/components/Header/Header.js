731a26f2cf55de560f6a33ea471809b483f2da02
Carlosedm97 <carlosedm97@gmail.com>
2023-04-24 22:56:47 -0500

    Trabajando en el componente header.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,6 +2,17 @@ import './Header.css'
 
 function Header(){
     return <header className='header'>
-        
+        <h1 className='header__logo'><a className='logo__enlace' href='https://github.com/Carlosedm97' rel='noopener'>CLDev<span className='logo__dot'>.</span></a></h1>
+        <nav className='header__nav'>
+            <ul className='nav__list'>
+                <li className='list__item'><a className='item__link' href='#' rel='noopener'>Sobre mi</a></li>
+                <li className='list__item'><a className='item__link' href='#' rel='noopener'>Hábilidades</a></li>
+                <li className='list__item'><a className='item__link' href='#' rel='noopener'>Formación</a></li>
+                <li className='list__item'><a className='item__link' href='#' rel='noopener'>Proyectos</a></li>
+                <li className='list__item'><a className='item__link' href='#' rel='noopener'>Contacto</a></li>
+            </ul>
+        </nav>
     </header>
 }
+
+export default Header
