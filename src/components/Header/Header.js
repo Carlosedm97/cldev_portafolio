682bf23bae3eb99b5323d03588fb6ef2ecfb50b9
Carlosedm97 <carlosedm97@gmail.com>
2023-04-25 14:33:58 -0500

    Finalizado el componente del Header e iniciando con el componente Hero.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import './Header.css'
 
 function Header(){
     return <header className='header'>
-        <h1 className='header__logo'><a className='logo__enlace' href='https://github.com/Carlosedm97' rel='noopener'>CLDev<span className='logo__dot'>.</span></a></h1>
+        <h1 className='header__logo'><a className='logo__enlace' href='https://github.com/Carlosedm97' rel='noreferrer'>CLDev<span className='logo__dot'>.</span></a></h1>
         <nav className='header__nav'>
             <ul className='nav__list'>
                 <li className='list__item'><a className='item__link' href='#' rel='noopener'>Sobre mi</a></li>
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,34 @@
+import './Hero.css'
+
+function Hero(){
+    return <section className='hero'>
+        <div className='hero__info'>
+            <div className='hero__texts'>
+                <p className='texts__paragraph'>Hola, me llamo Carlos, soy</p>
+                <h2 className='texts__title'>Desarrollador Front End.</h2>
+            </div>
+            <div className='hero__buttons'>
+                <a 
+                    className='button__link' 
+                    href='https://github.com/Carlosedm97' 
+                    target='_blank' 
+                    rel='noreferrer'>GitHub<i class="fa-brands fa-github"></i></a>
+                <a 
+                    className='button__link' 
+                    href='https://www.linkedin.com/in/carlos-antonio-meza-lopez/' 
+                    target='_blank' 
+                    rel='noreferrer'>LinkedIn<i class="fa-brands fa-linkedin"></i></a>
+                <a 
+                    className='button__link' 
+                    href='https://github.com/Carlosedm97' 
+                    target='_blank' 
+                    rel='noreferrer'>Curriculum<i class="fa-solid fa-file"></i></a>
+            </div>
+        </div>
+        <div className='hero__image'>
+            <img src='https://github.com/Carlosedm97.png' alt='Foto de Carlos'/>
+        </div>
+    </section>
+}
+
+export default Hero
