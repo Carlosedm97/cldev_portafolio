32501cb94c3783baab866f387e0a3e638347218a
Carlosedm97 <carlosedm97@gmail.com>
2023-06-03 12:45:48 -0500

    Actualizando la información del componente 'Sobre mi'.

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,7 +3,7 @@ import './AboutMe.css';
 const AboutMe = () => {
     return <section className='AboutMe' id='sobre-mi'>
         <h2 className='AboutMe__title'><span className='quotes'>{'< '}</span>Sobre mi<span className='quotes'>{' >'}</span></h2>
-        <p className='AboutMe__paragraph'>Desarrollador web especializado en el área Fron End con conocimiento en tecnologías como HTML5, CSS3, Javascript, React Js y el software de control de versiones Git. Adicionalmente, me he desempeñado en el sector salud como auxiliar de enfermería en un centro de bienestar para adultos mayores. En cuanto a mis habilidades blandas, las que he venido trabajando y aplicando son: la organización, el trabajo colaborativo, la empatía, gestión efectiva del tiempo, capacidad de respuesta, entre otras.</p>
+        <p className='AboutMe__paragraph'>Desarrollador web especializado en el área Front End con conocimiento en tecnologías como HTML5, CSS3, Javascript, React Js y el software de control de versiones Git. Adicionalmente, me he desempeñado en el sector salud como auxiliar de enfermería en un centro de bienestar para adultos mayores. En cuanto a mis habilidades blandas, las que he venido trabajando y aplicando son: la organización, el trabajo colaborativo, la empatía, gestión efectiva del tiempo, capacidad de respuesta, entre otras.</p>
     </section>
 };
 
